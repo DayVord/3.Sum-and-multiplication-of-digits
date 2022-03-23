bf5961970c0f52e3af45cc88197a0f57fc03f7f0
Dima Logvinets <mister.dayvord@gmail.com>
2022-03-23 18:13:58 +0300

    finally!)

--- Sum and multiplication of digits/Program.cs
+++ Sum and multiplication of digits/Program.cs
@@ -8,41 +8,17 @@ namespace Sum_and_multiplication_of_digits
         {
             Console.WriteLine("How many numbers do you want to use?");
             int numbersAmount = 0;
+            bool calculateNext = false;
 
-            for (int x = 0; ; x++)
+            while (!calculateNext)
             {
-                if (Int32.TryParse(Console.ReadLine(), out numbersAmount))
+                try
                 {
-                    int[] numbers = new int[numbersAmount];
-
-                    for (int i = 0; i < numbersAmount; i++)
-                    {
-                        Console.WriteLine();
-                        Console.Write($"Enter the {i + 1}st number:\t");
-                        numbers[i] = Convert.ToInt32(Console.ReadLine());
-                    }
-
-                    int sum = 0;
-                    for (int i = 0; i < numbersAmount; i++)
-                    {
-                        sum += numbers[i];
-                    }
-
-                    int product = 1;
-                    for (int i = 0; i < numbersAmount; i++)
-                    {
-                        product *= numbers[i];
-                    }
-
-                    Console.WriteLine();
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine($"The sum of numbers is {sum}");
-
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"The product of numbers is {product}");
+                    numbersAmount = Convert.ToInt32(Console.ReadLine());
+                    calculateNext = true;
                     break;
                 }
-                else
+                catch (Exception)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write("You had to enter a numeric value");
@@ -50,6 +26,51 @@ namespace Sum_and_multiplication_of_digits
                     Console.ResetColor();
                 }
             }
+
+            int[] numbers = new int[numbersAmount];
+
+            for (int x = 0; x < numbersAmount; x++)
+            {
+                Console.WriteLine();
+                Console.Write($"Enter the {x + 1}st number:\t");
+            reEnter:
+                try
+                {
+
+                    numbers[x] = Convert.ToInt32(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("You had to enter a numeric value");
+                    Console.WriteLine();
+                    Console.ResetColor();
+                    goto reEnter;
+                }
+
+            }
+
+            int sum = 0;
+            for (int i = 0; i < numbersAmount; i++)
+            {
+                sum += numbers[i];
+            }
+
+            int product = 1;
+            for (int i = 0; i < numbersAmount; i++)
+            {
+                product *= numbers[i];
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine($"The sum of numbers is {sum}");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"The product of numbers is {product}");
+
             Console.ReadKey();
         }
     }
